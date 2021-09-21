87e7cf1108ce4f80e422bbfae49f5b2036ffb8b7
Cogentx <code@cogentx.com>
2021-09-21 18:15:28 -0230

    Use Context data in Transactions - useContext hook

---
+++ src/components/Transaction.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Transaction({ transaction }) {
+
+const sign = transaction.amount >= 0 ? '+' : '-';
+
+  return (
+    <>
+      <li className={transaction.amount > 0 ? 'plus' : 'minus'}>
+            {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
+            <button className="delete-btn">x</button>
+          </li>
+    </>
+  )
+}
+
+export default Transaction
