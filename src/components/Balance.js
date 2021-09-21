64f2171519849ab2f5367a78e1ea51bf9c7a9393
Cogentx <code@cogentx.com>
2021-09-21 19:14:23 -0230

    Use Context data in Balance component - useContext

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,8 +1,17 @@
+import { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
 function Balance() {
+
+  const {transactions} = useContext(GlobalContext);
+  const balance = transactions.reduce((acc, t) => {
+    return acc + t.amount;
+  },0);
+
   return (
     <>
       <h4>Your Balance</h4>
-      <h1 id="balance">$0.00</h1>
+      <h1 id="balance">${balance.toFixed(2)}</h1>
     </>
   )
 }
